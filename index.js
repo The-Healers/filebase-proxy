7d531207bc232312083974d7efa09ecf9db8bfad
Chris Pickett <chris@Chriss-MacBook-Pro.local>
2022-03-05 20:49:10 -0500

    add unique ids

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const _ = require('lodash');
 
+const { v4: uuidv4 } = require('uuid');
+
 
 const app = express();
 
@@ -64,101 +66,104 @@ app.get('/', async (req, res) => {
 
 app.post('/upload', async (req, res) => {
   try {
-      if(!req.files) {
-          res.send({
+    const uid = uuidv4();
+    if(!req.files) {
+        res.send({
+            status: false,
+            message: 'No file uploaded'
+        });
+        console.log('No file uploaded')
+    } else {
+        //Use the name of the input field (i.e. "docFile") to retrieve the uploaded file
+        let docFile = req.files.docFile;
+        
+        //Use the mv() method to place the file in upload directory (i.e. "uploads")
+        docFile.mv('./uploads/' + docFile.name, (err) => {
+          if(err) {
+            console.log(err)
+            //send response
+            res.send({
               status: false,
-              message: 'No file uploaded'
-          });
-      } else {
-          //Use the name of the input field (i.e. "docFile") to retrieve the uploaded file
-          let docFile = req.files.docFile;
+              message: 'Failed moving file',
+              data: {
+                error: err
+              }
+            });
+            console.log("Failed moving file")
+          } else {
+
+            AWS.config.update({
+              accessKeyId: process.env.ACCESS_KEY,
+              secretAccessKey: process.env.SECRET_KEY,
+              signatureVersion: 'v4',
+            });
           
-          //Use the mv() method to place the file in upload directory (i.e. "uploads")
-          docFile.mv('./uploads/' + docFile.name, (err) => {
-            if(err) {
-              console.log(err)
-              //send response
-              res.send({
-                status: false,
-                message: 'Failed moving file',
-                data: {
-                  error: err
-                }
-              });
-            } else {
+            const s3 = new AWS.S3({
+                endpoint: process.env.ENDPOINT,
+            });
+          
+            // Read content from the file
+            const fileContent = fs.readFileSync('./uploads/' + docFile.name);
+          
+            s3.listBuckets(function(err, data) {
+              if (err) {
+                console.log(err, err.stack);
 
-              AWS.config.update({
-                accessKeyId: process.env.ACCESS_KEY,
-                secretAccessKey: process.env.SECRET_KEY,
-                signatureVersion: 'v4',
-              });
-            
-              const s3 = new AWS.S3({
-                  endpoint: process.env.ENDPOINT,
-              });
-            
-              // Read content from the file
-              const fileContent = fs.readFileSync('./uploads/' + docFile.name);
-            
-              s3.listBuckets(function(err, data) {
-                console.log("working")
-                if (err) {
-                  console.log(err, err.stack);
+                //send response
+                res.send({
+                  status: false,
+                  message: 'No Bucket',
+                  data: {
+                    error: err
+                  }
+                });
+                console.log('No Bucket')
+              } else {
+                // Setting up S3 upload parameters
+                var params = {
+                  Body: fileContent,
+                  Bucket: process.env.BUCKET_NAME,
+                  Key: uid,
+                };
+                s3.putObject(params, function(err, data) {
+                  if (err) {
+                    console.log(err, err.stack);
+                    //send response
+                    res.send({
+                      status: false,
+                      message: 'Failed uploading file',
+                      data: {
+                        error: err
+                      }
+                    });
+                    console.log('Failed uploading file')
+                  } else {
+                    console.log(data);
 
-                  //send response
-                  res.send({
-                    status: false,
-                    message: 'No Bucket',
-                    data: {
-                      error: err
-                    }
-                  });
-                } else {
-                  // Setting up S3 upload parameters
-                  var params = {
-                    Body: fileContent,
-                    Bucket: process.env.BUCKET_NAME,
-                    Key: docFile.name,
-                  };
-                  s3.putObject(params, function(err, data) {
-                    if (err) {
-                      console.log(err, err.stack);
-                      //send response
-                      res.send({
-                        status: false,
-                        message: 'Failed uploading file',
-                        data: {
-                          error: err
-                        }
-                      });
-                    } else {
-                      console.log(data);
+                    //send response
+                    res.send({
+                      status: true,
+                      message: 'File is uploaded',
+                      data: {
+                          uid
+                      }
+                    });
+                    console.log('File is uploaded')
 
-                      //send response
-                      res.send({
-                        status: true,
-                        message: 'File is uploaded',
-                        data: {
-                            name: docFile.name,
-                            mimetype: docFile.mimetype,
-                            size: docFile.size
-                        }
-                      });
-
-                      // Delete example_file.txt
-                      fs.unlink(`${'./uploads/' + docFile.name}`, (err => {
-                        if (err) { console.log("FAILED DELETING FILE", err) }
-                        else {
-                          console.log("Deleted file:", docFile.name);
-                        }
-                      }));
-                    }
-                  });
-                }
-              });
-            }
-          })
-      }
+                    // Delete example_file.txt
+                    fs.unlink(`${'./uploads/' + docFile.name}`, (err => {
+                      if (err) { console.log("FAILED DELETING FILE", err) }
+                      else {
+                        console.log("Deleted file:", docFile.name);
+                      }
+                    }));
+                  }
+                });
+              }
+            });
+          }
+        })
+    }
   } catch (err) {
       res.status(500).send(err);
   }
@@ -168,7 +173,7 @@ app.post('/upload', async (req, res) => {
 app.use(express.static('uploads'));
 
 //start app 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => 
   console.log(`App is listening on port ${port}.`)
